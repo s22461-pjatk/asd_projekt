de11929f3a9daad438e6e21f52805c4fd3787ed0
s22461 <84182653+s22461-pjatk@users.noreply.github.com>
2021-11-30 09:26:18 +0100

    Reopload

--- mergesort.py
+++ mergesort.py
@@ -1,40 +1,34 @@
 class Mergesort():
-    def mergesort(self, myList):
-        if len(myList) > 1:
-            mid = len(myList) // 2
-            left = myList[:mid]
-            right = myList[mid:]
+    def mergesort(self, A):
+        if len(A) > 1:
+            mid = len(A) // 2
+            left = A[:mid]
+            right = A[mid:]
 
-        # Recursive call on each half
             self.mergesort(left)
             self.mergesort(right)
 
-        # Two iterators for traversing the two halves
             i = 0
             j = 0
-        
-        # Iterator for the main list
+
             k = 0
         
             while i < len(left) and j < len(right):
                 if left[i] <= right[j]:
-              # The value from the left half has been used
-                    myList[k] = left[i]
-              # Move the iterator forward
+                    A[k] = left[i]
                     i += 1
                 else:
-                    myList[k] = right[j]
+                    A[k] = right[j]
                     j += 1
-            # Move to the next slot
+
                 k += 1
 
-        # For all the remaining values
             while i < len(left):
-                myList[k] = left[i]
+                A[k] = left[i]
                 i += 1
                 k += 1
 
             while j < len(right):
-                myList[k]=right[j]
+                A[k]=right[j]
                 j += 1
                 k += 1
